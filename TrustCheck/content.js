a16967ec90a8d1b1884f08076b9c245f0213d4ee
Meghna Varma <114867153+meghnavarma25@users.noreply.github.com>
2024-11-05 17:07:34 +0530

    Delete TrustCheck directory

--- TrustCheck/content.js
+++
@@ -1,8 +0,0 @@
-let pageText = document.body.innerText;
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "extractText") {
-    sendResponse({ text: pageText });
-  }
-});
-
